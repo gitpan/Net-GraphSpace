c5bbbe9b199488f31004f4e7df65aec47441e7f4
Naveed Massjouni <naveed@vt.edu>
2011-10-31 13:03:07 +0000

    Import of IRONCAMEL/Net-GraphSpace-0.0001 from CPAN.
    gitpan-cpan-distribution: Net-GraphSpace
    gitpan-cpan-version:      0.0001
    gitpan-cpan-path:         IRONCAMEL/Net-GraphSpace-0.0001.tar.gz
    gitpan-cpan-author:       IRONCAMEL
    gitpan-cpan-maturity:     released

---
+++ lib/Net/GraphSpace/AttributesToJSON.pm
@@ -0,0 +1,47 @@
+package Net::GraphSpace::AttributesToJSON;
+use Moose::Role;
+use MooseX::Method::Signatures;
+use v5.10;
+
+method TO_JSON() {
+    my @attrs = $self->meta->get_all_attributes;
+    return { map $self->_affinitize($_), @attrs };
+}
+
+method _affinitize($attr) {
+    my $name = $attr->name;
+    my $value = $self->$name;
+    return if not defined $value;
+    given ($attr->type_constraint) {
+        when ($_->equals('Str')) { "$value"   }
+        when ($_->equals('Int')) { int $value }
+    }
+    return $name => $value;
+}
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Net::GraphSpace::AttributesToJSON
+
+=head1 VERSION
+
+version 0.0001
+
+=head1 AUTHOR
+
+Naveed Massjouni <naveedm9@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Naveed Massjouni.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
